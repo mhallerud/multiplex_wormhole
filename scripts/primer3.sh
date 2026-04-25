2f5efb18b4eb66c637d53d8c0a02d655dc30df7b
mhallerud <hallerum@oregonstate.edu>
2026-04-24 21:35:18 -0700

    improve primer3 setting handling

--- scripts/primer3.sh
+++ scripts/primer3.sh
@@ -17,6 +17,6 @@ sed 's/SEQUENCE_TARGET=/SEQUENCE_TARGET='"$TARGET"'/' > $OUTDIR/primer3_settings
 # design primers with primer3
 #primer3_core [ --format_output ] [--default_version=1|--default_version=2] [ --io_version=4 ] [ --p3_settings_file=<file_path> ] [ --echo_settings_file ] [ --strict_tags ] [ --output=<file_path> ] [ --error=<file_path> ] [ input_file ]
 
-$PRIMER3_PATH $SCRIPTPATH/../primer3_settings/primer3_settings.txt --error="$OUTDIR"/"$ID".err --output="$OUTDIR"/"$ID".out
+$PRIMER3_PATH $OUTDIR/primer3_settings.txt --error="$OUTDIR"/"$ID".err --output="$OUTDIR"/"$ID".out
 
 rm $OUTDIR/primer3_settings.txt
