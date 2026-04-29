5156ed16667f9eaeb9e92344b12b67b90026b487
mhallerud <hallerum@oregonstate.edu>
2026-04-29 00:59:02 -0700

    fixing init for module loading and function names

--- src/multiplex_wormhole/__init__.py
+++ src/multiplex_wormhole/__init__.py
@@ -1,16 +1,16 @@
 # LOAD ALL SUBMODULES
-import sys, os, importlib, glob, types
+import sys, os, importlib, glob
 sys.path.append(os.path.dirname(__file__))
 
-from ./multiplex_wormhole import main as multiplex_wormhole
-from . import panel_assessment
-from . import setup_mfeprimer
-from . import batch_primer3_design
-from . import tabulate_dimers
-from . import optimize_multiplex 
-from . import multiple_run_optimization 
-from . CSVtoFasta
-from . import add_keeplist_to_fasta
+import multiplexWormhole
+import panel_assessment
+import setup_mfeprimer
+import batch_primer3_design
+import tabulate_dimers
+import optimize_multiplex 
+import multiple_run_optimization 
+import CSVtoFasta
+import add_keeplist_to_fasta
 plot_ASA_temps = importlib.import_module("plot_ASA_temps")
 
 
@@ -22,9 +22,9 @@ tabulateDimers = tabulate_dimers.main
 plotASAtemps = plot_ASA_temps.main
 optimizeMultiplex = optimize_multiplex.main
 multipleOptimizations = multiple_run_optimization.main
-CSVtoFASTA = CSVtoFASTA.main
+CSVtoFASTA = CSVtoFasta.main
 addKeeplist2FASTA = add_keeplist_to_fasta.main
-
+multiplexWormhole = multiplexWormhole.main
 
 
 # SET DEPENDENCY PATH FOR MFEPRIMER
@@ -32,5 +32,5 @@ MFEprimer_PATH = glob.glob(os.path.dirname(__file__)+"/*mfeprimer*")[0]
 
 
 
-__all__ = ["multiplex_wormhole", "panel_assessment", "setup_mfeprimer", "batch_primer3_design", "tabulate_dimers", "optimize_multiplex", "multiple_run_optimization", "CSVtoFasta", "add_keeplist_to_fasta", "plot_ASA_temps"]
+__all__ = ["multiplexWormhole", "panel_assessment", "setup_mfeprimer", "batch_primer3_design", "tabulate_dimers", "optimize_multiplex", "multiple_run_optimization", "CSVtoFasta", "add_keeplist_to_fasta", "plot_ASA_temps"]
 #, "assessPanel", "primer3BatchDesign", "tabulateDimers", "plotASAtemps", "optimizeMultiplex", "multipleOptimizations", "CSVtoFASTA", "addKeeplist2FASTA", "MFEprimer_PATH"]
