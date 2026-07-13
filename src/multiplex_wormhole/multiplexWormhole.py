c64fbdbc9601f70975c2445d2ba9b285743fe12b
mhallerud <hallerum@oregonstate.edu>
2026-07-12 17:43:31 -0700

    fix input CSV2fasta filepath at final step

--- src/multiplex_wormhole/multiplexWormhole.py
+++ src/multiplex_wormhole/multiplexWormhole.py
@@ -326,7 +326,7 @@ def main(TEMPLATES, N_LOCI, OUTDIR, PREFIX=None, KEEPLIST_FA=None, N_RUNS=10,
             mwlogger.info("Run %s had %s dimers.", str(run[i]), str(dimers[i]))
             if dimers[i]==min(dimers):
                 mwlogger.info(".....Converting to FASTA for additional screening")
-                CSVtoFASTA(IN_CSV = os.path.join(OUTDIR3,"Final_Primers", PREFIX+"_Run"+str(i)+"_primers.csv"), 
+                CSVtoFASTA(IN_CSV = os.path.join(OUTDIR3,"Final_Primers", PREFIX+"_"+run[i]+"_primers.csv"), 
                            OUT_FA = os.path.join(OUTDIR3,PREFIX+run[i]+"_primers.fasta"))
     else: 
         mwlogger.warning("RunSummary not found- optimization step may have failed.")
